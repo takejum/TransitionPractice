088d5a5fe32c847e5cd0b30382f9913c8f601211
Jumpei Takeshita <takejum.123@gmail.com>
2020-03-14 00:43:00 +0900

    Initial Commit

---
+++ transitionTrainingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  transitionTrainingApp
+//
+//  Created by Jumpei Takeshita on 2020/03/14.
+//  Copyright © 2020 Jumpei Takeshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
