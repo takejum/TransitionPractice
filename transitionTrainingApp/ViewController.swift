3980eedb7354ddf0f1f40ea1a5a160edd26cbe3a
takejum <takejum.123@gmail.com>
2020-03-15 14:53:46 +0900

    Update ViewController.swift

--- transitionTrainingApp/ViewController.swift
+++ transitionTrainingApp/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
-
+    @IBAction func nextButton(_ sender: Any) {
+        
+        count += 1
+        
+        numberLabel.text = String(count)
+        
+        if count == 10{
+            count = 0
+//            performSegue(withIdentifier: "Next", sender: nil)
+            
+            
+            //defining secondVC variable will be used in the code which will go to next view.
+            let secondVC =
+                storyboard?.instantiateViewController(identifier: "Next") as! SecondViewController
+            
+            //putting the value of count in count2 in the SecondViewController
+            secondVC.count2 = count
+            
+            //going to next view using navigation controller
+            navigationController?.pushViewController(secondVC, animated: true)
+            
+        }
+        
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let secondVC = segue.destination as! SecondViewController
+    
+    
+//        secondVC.count2 = count
+    }
